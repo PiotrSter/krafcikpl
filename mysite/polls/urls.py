f8e63ebf2a0637e1d28d68e2055075d8e40b869f
PiotrSter <47736359+PiotrSter@users.noreply.github.com>
2020-12-10 14:58:55 +0100

    views

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path, include
 from django.conf.urls import url
-from .views import UserList, UserDetail
+from .views import UserList, UserDetail, SupplierList, SupplierDetail, BreweryList, BreweryDetail, TypeList, \
+    TypeDetail, ColorList, ColorDetail, CapacityList, CapacityDetail, BeerList, BeerDetail, ShopList, ShopDetail, \
+    OrderList, OrderDetail
 
 urlpatterns = [
     url(r'^api-auth/', include('rest_framework.urls')),
     path('', UserList.as_view()),
     path('<int:pk>/', UserDetail.as_view()),
+    path('', SupplierList.as_view()),
+    path('<int:pk>/', SupplierDetail.as_view())
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -10,3 +10,67 @@ class UserList(generics.ListCreateAPIView):
 class UserDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+class SupplierList(generics.ListCreateAPIView):
+    queryset = Supplier.objects.all()
+    serializer_class = SupplierSerializer
+
+class SupplierDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Supplier.objects.all()
+    serializer_class = SupplierSerializer
+
+class BreweryList(generics.ListCreateAPIView):
+    queryset = Brewery.objects.all()
+    serializer_class = BrewerySerializer
+
+class BreweryDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Brewery.objects.all()
+    serializer_class = BrewerySerializer
+
+class TypeList(generics.ListCreateAPIView):
+    queryset = Type.objects.all()
+    serializer_class = TypeSerializer
+
+class TypeDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Type.objects.all()
+    serializer_class = TypeSerializer
+
+class ColorList(generics.ListCreateAPIView):
+    queryset = Color.objects.all()
+    serializer_class = ColorSerializer
+
+class ColorDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Color.objects.all()
+    serializer_class = ColorSerializer
+
+class CapacityList(generics.ListCreateAPIView):
+    queryset = Capacity.objects.all()
+    serializer_class = ColorSerializer
+
+class CapacityDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Capacity.objects.all()
+    serializer_class = CapacitySerializer
+
+class BeerList(generics.ListCreateAPIView):
+    queryset = Beer.objects.all()
+    serializer_class = BeerSerializer
+
+class BeerDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Beer.objects.all()
+    serializer_class = BeerSerializer
+
+class ShopList(generics.ListCreateAPIView):
+    queryset = Shop.objects.all()
+    serializer_class = ShopSerializer
+
+class ShopDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Shop.objects.all()
+    serializer_class = ShopSerializer
+
+class OrderList(generics.ListCreateAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+class OrderDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
