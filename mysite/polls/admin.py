bea76e27769088d2002ee66a4bc91d183f799426
KornelStanulewicz <kornel.stanulewicz@gmail.com>
2020-12-16 16:53:15 +0100

    cd

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,14 +1,14 @@
 from django.contrib import admin
-# from mysite.polls.models import User, Supplier, Brewery, Type, Color, Capacity, Beer, Shop, Order
+from .models import User, Supplier, Brewery, Type, Color, Capacity, Beer, Shop, Order
 
 # Register your models here.
 
-# admin.site.register(User)
-# admin.site.register(Supplier)
-# admin.site.register(Brewery)
-# admin.site.register(Type)
-# admin.site.register(Color)
-# admin.site.register(Capacity)
-# admin.site.register(Beer)
-# admin.site.register(Shop)
-# admin.site.register(Order)
+admin.site.register(User)
+admin.site.register(Supplier)
+admin.site.register(Brewery)
+admin.site.register(Type)
+admin.site.register(Color)
+admin.site.register(Capacity)
+admin.site.register(Beer)
+admin.site.register(Shop)
+admin.site.register(Order)
