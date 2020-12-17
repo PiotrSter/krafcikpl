f8735852ea508d745099c0211f98c45671612e41
KornelStanulewicz <kornel.stanulewicz@gmail.com>
2020-12-17 18:39:48 +0100

    cd

--- mysite/polls/serializers.py
+++ mysite/polls/serializers.py
@@ -2,47 +2,54 @@ from rest_framework import serializers
 from .models import User, Supplier, Brewery, Type, Color, Capacity, Beer, Shop, Order
 
 
-class UserSerializer(serializers.ModelSerializer):
+class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
         fields = ['login', 'email', 'password', 'address', 'phone_number']
 
-class SupplierSerializer(serializers.ModelSerializer):
+class SupplierSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Supplier
         fields = ['name', 'surname', 'vechicle']
 
-class BrewerySerializer(serializers.ModelSerializer):
+class BrewerySerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Brewery
         fields = ['brewery_name']
 
-class TypeSerializer(serializers.ModelSerializer):
+class TypeSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Type
         fields = ['type_of_beer']
 
-class ColorSerializer(serializers.ModelSerializer):
+class ColorSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Color
         fields = ['color']
 
-class CapacitySerializer(serializers.ModelSerializer):
+class CapacitySerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Capacity
         fields = ['capacity']
 
-class BeerSerializer(serializers.ModelSerializer):
+class BeerSerializer(serializers.HyperlinkedModelSerializer):
+    brewery = serializers.SlugRelatedField(queryset=Brewery.objects.all(), slug_field='brewery_name')
+    type = serializers.SlugRelatedField(queryset=Type.objects.all(), slug_field='type_of_beer')
+    color = serializers.SlugRelatedField(queryset=Color.objects.all(), slug_field='color')
+    capacity = serializers.SlugRelatedField(queryset=Capacity.objects.all(), slug_field='capacity')
+
     class Meta:
         model = Beer
         fields = ['name', 'brewery', 'type', 'color', 'capacity']
 
-class ShopSerializer(serializers.ModelSerializer):
+class ShopSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Shop
         fields = ['name', 'beer', 'price']
 
-class OrderSerializer(serializers.ModelSerializer):
+class OrderSerializer(serializers.HyperlinkedModelSerializer):
+    supplier = serializers.SlugRelatedField(queryset=Supplier.objects.all(), slug_field='name')
+
     class Meta:
         model = Order
         fields = ['user', 'shop', 'supplier']
